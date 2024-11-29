70b476c7f0c3d4d6404f33a02c3068162cf4b0ea
4R1u <62248461+4R1u@users.noreply.github.com>
2024-11-29 18:03:32 +0400

    Create Game Class with Basic Variables

--- lib/game.rb
+++ lib/game.rb
@@ -1,1 +1,11 @@
 # frozen_string_literal: true
+
+# This represents an instance of the hangman game.
+class Game
+  def initialize(word)
+    @word = word
+    @guesses_left = 8
+    @state = '_' * word.size
+    @wrong_letters = ''
+  end
+end
