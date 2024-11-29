25cc1243b08f53ca21e0e14916ecd9910fefea02
4R1u <62248461+4R1u@users.noreply.github.com>
2024-11-29 18:04:23 +0400

    Make All Game Vars Readable Except word

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,8 @@
 
 # This represents an instance of the hangman game.
 class Game
+  attr_reader :guesses_left, :state, :wrong_letter
+
   def initialize(word)
     @word = word
     @guesses_left = 8
