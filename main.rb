2994d3417e9abc66fe87cf96859821c6324bd679
4R1u <62248461+4R1u@users.noreply.github.com>
2024-11-30 17:07:33 +0400

    Print Instructions and Win/Loss Messages

--- main.rb
+++ main.rb
@@ -9,9 +9,13 @@ while (line = words_file.gets)
   words << line if (5..12).include?(line.length)
 end
 
+puts 'The game will choose a random word between 5 and 12 words long.'
 game = Game.new(words.sample)
 until game.over?
+  print "Enter a letter to see if it\'s in the word,"
+  puts ' or a word itself you think is the answer.'
   game.attempt(gets.chomp)
   puts game.state
   p game.tried_strings
 end
+puts game.won? ? 'You won!' : 'You lost!'
