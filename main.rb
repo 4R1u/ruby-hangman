0f4aed7a84a458f6b51ed23f528bac8b6d9bf4d9
4R1u <62248461+4R1u@users.noreply.github.com>
2024-11-30 12:15:23 +0400

    Filter Out Words Not Between 5-12 Letter Long

--- main.rb
+++ main.rb
@@ -5,7 +5,8 @@ require_relative 'lib/game'
 words_file = File.open('resources/google-10000-english-no-swears.txt')
 words = []
 while (line = words_file.gets)
-  words << line.chomp
+  line.chomp!
+  words << line if (5..12).include?(line.length)
 end
 
 game = Game.new(words.sample)
