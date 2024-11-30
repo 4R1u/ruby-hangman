ddeb9a6f944d1195058f9d496cb58107e5f7c39a
4R1u <62248461+4R1u@users.noreply.github.com>
2024-11-30 11:50:43 +0400

    Basic Game Loop

--- main.rb
+++ main.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
+require_relative 'lib/game'
+
 words_file = File.open('resources/google-10000-english-no-swears.txt')
 words = []
 while (line = words_file.gets)
   words << line.chomp
+end
+
+game = Game.new(words.sample)
+until game.over?
+  game.attempt(gets.chomp)
+  puts game.state
+  puts game.tried_letters
 end
